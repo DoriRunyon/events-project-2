d4c8628cc36cd820c009845caaec9857a217f7d2
DoriRunyon <dorileerunyon@gmail.com>
2017-02-05 15:53:35 -0800

    Added base and index templates.

--- views.py
+++ views.py
@@ -3,5 +3,5 @@ from django.http import HttpResponse
 
 
 def index(request):
-    return HttpResponse("Hello, world. You're at the events index.")
-
+    return render(request, 'events/index.html')
+    #return HttpResponse("hello ther")
